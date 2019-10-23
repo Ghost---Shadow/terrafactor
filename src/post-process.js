c4a5a67782825dbe8655e9e62d3e9e7f59370e9b
Souradeep Nanda <souradeep.nanda123@gmail.com>
2019-10-23 15:22:19 +0530

    Var file generation

--- src/post-process.js
+++ src/post-process.js
@@ -89,6 +89,61 @@ const filterOnlyIdsLut = (lut) => Object.keys(lut).reduce((acc, key) => {
   return acc;
 }, {});
 
+const filterOnlyNotIdsLut = (lut, dryness = 2) => Object.keys(lut).reduce((acc, key) => {
+  const ids = lut[key].filter((maybeId) => maybeId.endsWith('.id'));
+  // DRY: Dont repeat yourself
+  const isWetEnough = lut[key].length >= dryness;
+  const isIdReference = !!ids.length;
+  if (!isIdReference && isWetEnough) return { ...acc, [key]: lut[key] };
+  return acc;
+}, {});
+
+const varToValueLut = (lut) => {
+  const valueLut = Object.keys(lut)
+    .reduce((acc, key) => {
+      const parts = lut[key][0].split('.');
+      const name = parts[parts.length - 1];
+      // Trailing underscore is to disambiguate from
+      // keywords like count or version
+      let nonConflictingName = `${name}_`;
+      let i = 1;
+      while (acc[nonConflictingName]) {
+        nonConflictingName = `${name}_${i}`;
+        i += 1;
+      }
+      return {
+        ...acc,
+        [nonConflictingName]: key,
+      };
+    }, {});
+
+  // Sort the keys for git stability
+  const sortedLut = Object.keys(valueLut).sort()
+    .reduce((acc, key) => ({ ...acc, [key]: valueLut[key] }), {});
+
+  // Invert the mapping
+  return Object.keys(sortedLut)
+    .reduce((acc, key) => ({ ...acc, [sortedLut[key]]: key }), {});
+};
+
+const valueLutToVarFile = (valueLut) => Object.keys(valueLut)
+  .reduce((acc, key) => `${acc}
+variable "${valueLut[key]}" {
+  type = string
+  default = "${key}"
+}
+`, '');
+
+const mergeLuts = (idsLut, valueLut) => {
+  const varLut = Object.keys(valueLut)
+    .reduce((acc, key) => ({
+      ...acc,
+      [key]: `var.${valueLut[key]}`,
+    }), {});
+
+  return { ...idsLut, ...varLut };
+};
+
 const postProcess = async (generatedDir, outputDir) => {
   const statePath = path.join(generatedDir, 'terraform.tfstate');
   const stateJson = JSON.parse(fs.readFileSync(statePath).toString());
@@ -100,9 +155,12 @@ const postProcess = async (generatedDir, outputDir) => {
   const { resources } = stateJson;
 
   const inverseLut = genInverseLut(resources);
-  // TODO: Add support for other attributes
   const idsLut = filterOnlyIdsLut(inverseLut);
-  const regexLut = toRegexLut(idsLut);
+  const notIdsLut = filterOnlyNotIdsLut(inverseLut);
+  const valueLut = varToValueLut(notIdsLut);
+  const varFileString = valueLutToVarFile(valueLut);
+  const mergedLuts = mergeLuts(idsLut, valueLut);
+  const regexLut = toRegexLut(mergedLuts);
 
   const tfFileNames = (await readdir(generatedDir))
     .filter((maybeFile) => maybeFile.match(/(\.tf$|\.hcl$)/));
@@ -111,12 +169,14 @@ const postProcess = async (generatedDir, outputDir) => {
     const outputFilePath = path.join(outputDir, fileName);
     const tfFileContents = (await readFile(inputFilePath)).toString();
     const processedFile = processFile(tfFileContents, regexLut);
-    await writeFile(outputFilePath, processedFile);
+    return writeFile(outputFilePath, processedFile);
   });
 
-  const copyPromise = copyFile(path.join(generatedDir, 'terraform.tfstate'), path.join(outputDir, 'terraform.tfstate'));
+  const copyPromise = copyFile(path.join(generatedDir, 'terraform.tfstate'),
+    path.join(outputDir, 'terraform.tfstate'));
+  const varFilePromise = writeFile(path.join(outputDir, 'variables.tf'), varFileString);
 
-  await Promise.all([...replacePromises, copyPromise]);
+  return Promise.all([...replacePromises, copyPromise, varFilePromise]);
 };
 
 module.exports = {
